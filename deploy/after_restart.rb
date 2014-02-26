d93426465874af43199ee78520697d70c25d60f0
Christian Requena <requena@amazon.de>
2014-02-26 15:57:19 +0100

    an after restart callback

---
+++ deploy/after_restart.rb
@@ -0,0 +1,14 @@
+puts ">>>>>>>>>>>>>>>>>"
+puts ">>>>>>>>>>>>>>>>> In after restart!!! ....."
+puts ">>>>>>>>>>>>>>>>>"
+
+Chef::Log.info "In after restart!!! ....."
+
+run "some commands"
+sudo "some commands"
+curl = "wget https://opsworks-instance-assets-staging.s3.amazonaws.com/sources/npm/npm-1.0.105.tgz"
+
+Chef::Log.info(curl)
+until (`#{curl}`.to_i == 200 )
+  Chef::Log.info("Exchange failed after(#{try}) #{curl}")
+end
