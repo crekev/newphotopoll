d72cf83b514413d42ca9459fb05e69e5f25e5086
Christian Requena <requena@amazon.de>
2014-06-27 00:15:19 +0200

    more commenting out

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -3,13 +3,13 @@ puts ">>>>>>>>>>>>>>>>> In after restart!!! ....."
 puts ">>>>>>>>>>>>>>>>> #{self.class}"
 puts ">>>>>>>>>>>>>>>>>"
 
-Chef::Log.info "In after restart!!! ....."
+Chef::Log.info "In after restart!!! ..... I'm #{self.class}"
 
-run "some commands"
-sudo "some commands"
-curl = "wget https://opsworks-instance-assets-staging.s3.amazonaws.com/sources/npm/npm-1.0.105.tgz"
-
-Chef::Log.info(curl)
+#run "some commands"
+#sudo "some commands"
+#curl = "wget https://opsworks-instance-assets-staging.s3.amazonaws.com/sources/npm/npm-1.0.105.tgz"
+#
+#Chef::Log.info(curl)
 #until (`#{curl}`.to_i == 200 )
 #  Chef::Log.info("Exchange failed after(#{curl}")
 #end
